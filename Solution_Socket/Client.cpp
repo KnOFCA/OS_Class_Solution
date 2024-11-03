64bd5919f8aecb65c93b52b7d51333573728c0d8
KnOFCA <knofca@foxmail.com>
2024-11-03 19:09:51 +0800

    add gui for Solution_Socket

--- Solution_Socket/Client.cpp
+++ Solution_Socket/Client.cpp
@@ -1,51 +1,111 @@
 ﻿#include <iostream>
 #include <winsock2.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <GLFW/glfw3.h>
+#include <vector>
 
 #pragma comment(lib, "ws2_32.lib")
 
-int main() {
+SOCKET sock = INVALID_SOCKET;
+std::vector<std::string> messages;
+char inputBuffer[256] = "";
+
+void connectToServer() {
     WSADATA wsaData;
-    SOCKET sock;
     sockaddr_in servAddr;
-    char buffer[1024] = { 0 };
-    const char* message = "Hello from client";
 
-    // init Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "WSAStartup failed" << std::endl;
-        return -1;
-    }
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    // create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        std::cerr << "Socket creation failed" << std::endl;
-        WSACleanup();
-        return -1;
-    }
+    sock = socket(AF_INET, SOCK_STREAM, 0);
 
     servAddr.sin_family = AF_INET;
     servAddr.sin_port = htons(8080);
     servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // connect server
-    if (connect(sock, (struct sockaddr*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) {
-        std::cerr << "Connection failed" << std::endl;
-        closesocket(sock);
-        WSACleanup();
-        return -1;
+    connect(sock, (struct sockaddr*)&servAddr, sizeof(servAddr));
+}
+
+void sendMessage() {
+    if (strlen(inputBuffer) > 0) {
+        send(sock, inputBuffer, strlen(inputBuffer), 0);
+        messages.push_back("Client: " + std::string(inputBuffer));
+        memset(inputBuffer, 0, sizeof(inputBuffer));
+    }
+}
+
+void receiveMessages() {
+    char buffer[1024] = { 0 };
+    int valread = recv(sock, buffer, sizeof(buffer), 0);
+    if (valread > 0) {
+        messages.push_back("Server: " + std::string(buffer));
+    }
+}
+
+void renderUI() {
+    ImGui::Begin("Client");
+
+    if (ImGui::Button("Connect")) {
+        connectToServer();
+    }
+    ImGui::InputText("Message", inputBuffer, sizeof(inputBuffer));
+    if (ImGui::Button("Send Message")) {
+        sendMessage();
+    }
+    if (ImGui::Button("Receive Message")) {
+        receiveMessages();
     }
 
-    // send message
-    send(sock, message, strlen(message), 0);
-    std::cout << "Message sent from client" << std::endl;
+    ImGui::Text("Messages:");
+    for (const auto& msg : messages) {
+        ImGui::Text("%s", msg.c_str());
+    }
 
-    // receive message
-    int valread = recv(sock, buffer, sizeof(buffer), 0);
-    std::cout << "Message from server: " << buffer << std::endl;
+    ImGui::End();
+}
 
-    // close socket
-    closesocket(sock);
-    WSACleanup();
+void initOpenGL() {
+    if (!glfwInit())
+        return;
 
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui Client", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    glViewport(0, 0, 1280, 720);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    connectToServer();
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        renderUI();
+
+        ImGui::Render();
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+int main() {
+    initOpenGL();
     return 0;
 }
--- Solution_Socket/Server.cpp
+++ Solution_Socket/Server.cpp
@@ -1,72 +1,129 @@
 #include <iostream>
 #include <winsock2.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <GLFW/glfw3.h>
+#include <vector>
 
 #pragma comment(lib, "ws2_32.lib")
 
-int main() {
+SOCKET serverSocket = INVALID_SOCKET, clientSocket = INVALID_SOCKET;
+std::vector<std::string> messages;
+char inputBuffer[256] = "";
+
+
+void startServer() {
     WSADATA wsaData;
-    SOCKET serverSocket, clientSocket;
     sockaddr_in serverAddr, clientAddr;
     int addrLen = sizeof(clientAddr);
-    char buffer[1024] = { 0 };
     const char* hello = "Hello from server";
 
-    // init Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "WSAStartup failed" << std::endl;
-        return -1;
-    }
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    // create socket
-    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        std::cerr << "Socket creation failed" << std::endl;
-        WSACleanup();
-        return -1;
-    }
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // set addr struct
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(8080);
 
-    // bind socket
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        std::cerr << "Bind failed" << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return -1;
-    }
+    bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 
-    // lestion
-    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-        std::cerr << "Listen failed" << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return -1;
-    }
+    listen(serverSocket, SOMAXCONN);
 
-    std::cout << "Waiting for connections..." << std::endl;
+    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
 
-    // accept connection
-    if ((clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen)) == INVALID_SOCKET) {
-        std::cerr << "Accept failed" << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return -1;
-    }
-
-    // send message
     send(clientSocket, hello, strlen(hello), 0);
-    std::cout << "Hello message sent" << std::endl;
+}
 
-    // receive message
-    int valread = recv(clientSocket, buffer, sizeof(buffer), 0);
-    std::cout << "Message from client: " << buffer << std::endl;
-
-    // close socket
+void stopServer() {
     closesocket(clientSocket);
     closesocket(serverSocket);
     WSACleanup();
+}
 
+void sendMessage() {
+    if (strlen(inputBuffer) > 0) {
+        send(clientSocket, inputBuffer, strlen(inputBuffer), 0);
+        messages.push_back("Client: " + std::string(inputBuffer));
+        memset(inputBuffer, 0, sizeof(inputBuffer));
+    }
+}
+
+
+void receiveMessages() {
+    char buffer[1024] = { 0 };
+    int valread = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (valread > 0) {
+        messages.push_back("Client: " + std::string(buffer));
+    }
+}
+
+void renderUI() {
+    ImGui::Begin("Server");
+
+    if (ImGui::Button("Start Server")) {
+        startServer();
+    }
+    if (ImGui::Button("Stop Server")) {
+        stopServer();
+    }
+    ImGui::InputText("Message", inputBuffer, sizeof(inputBuffer));
+    if (ImGui::Button("Send Message")) {
+        sendMessage();
+    }
+
+    if (ImGui::Button("Receive Message")) {
+        receiveMessages();
+    }
+
+    ImGui::Text("Messages:");
+    for (const auto& msg : messages) {
+        ImGui::Text("%s", msg.c_str());
+    }
+
+    ImGui::End();
+}
+
+void initOpenGL() {
+    if (!glfwInit())
+        return;
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui Server", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    glViewport(0, 0, 1280, 720);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        renderUI();
+
+        ImGui::Render();
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+int main() {
+    initOpenGL();
     return 0;
 }
